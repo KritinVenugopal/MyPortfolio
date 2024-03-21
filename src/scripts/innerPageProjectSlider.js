0325a4abd7ec28a575042ee3c54b94cb83b7ecd7
suyog <suyogrhm+github@gmail.com>
2024-03-21 14:20:28 +0530

    inner page slider project section

--- src/scripts/innerPageProjectSlider.js
+++ src/scripts/innerPageProjectSlider.js
@@ -1,65 +1,33 @@
-// inner page project slider
-let slideIndex = 0;
-const slides = document.querySelectorAll('.card-out');
-const slideInterval = 5000; // 5 seconds
+let currentSlide = 0;
+    const slides = document.querySelectorAll('.card-out');
 
-function showSlides() {
-    slides.forEach(slide => {
-        slide.style.display = 'none';
-    });
-
-    slides[slideIndex].style.display = 'inline-block';
-
-    // Increment slide index for the next slide
-    slideIndex++;
-    // Reset slide index to 0 if it exceeds the number of slides
-    if (slideIndex >= slides.length) {
-        slideIndex = 0;
-    }
-}
-
-function nextSlide() {
-    if (slideIndex < slides.length - 1) {
-        slideIndex++;
-        showSlides();
+    // Function to show previous slide
+    function prevSlide() {
+        if (currentSlide > 0) {
+            currentSlide--;
+        } else {
+            currentSlide = slides.length - 1;
+        }
+        updateSlider();
     }
 
-    // Hide or show navigation buttons
-    toggleButtons();
-}
-
-function prevSlide() {
-    if (slideIndex > 0) {
-        slideIndex--;
-        showSlides();
+    // Function to show next slide
+    function nextSlide() {
+        if (currentSlide < slides.length - 1) {
+            currentSlide++;
+        } else {
+            currentSlide = 0;
+        }
+        updateSlider();
     }
 
-    // Hide or show navigation buttons
-    toggleButtons();
-}
-
-function toggleButtons() {
-    const prevButton = document.querySelector('.projectPrev');
-    const nextButton = document.querySelector('.projectNext');
-
-    // // Hide previous button on first slide
-    // if (slideIndex === 0) {
-    //     prevButton.style.display = 'none';
-    // } else {
-    //     prevButton.style.display = 'inline-block';
-    // }
-
-    // // Hide next button on last slide
-    // if (slideIndex === slides.length - 1) {
-    //     nextButton.style.display = 'none';
-    // } else {
-    //     nextButton.style.display = 'inline-block';
-    // }
-}
-
-// Show the first slide initially
-showSlides();
-// Hide or show navigation buttons initially
-toggleButtons();
-// Set interval for auto-sliding
-setInterval(showSlides, slideInterval);
+    // Function to update slider based on currentSlide
+    function updateSlider() {
+        slides.forEach((slide, index) => {
+            if (index === currentSlide) {
+                slide.style.display = 'block'; // Show current slide
+            } else {
+                slide.style.display = 'none'; // Hide other slides
+            }
+        });
+    }
